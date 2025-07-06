8a6ece321bfa1403f0a45ae63057f7667e7c7b91
kibedijoshua <kibedijoshua7@gmail.com>
2025-07-06 18:34:54 +0300

    up
    added the list view

--- yt_notes/noteapp/admin.py
+++ yt_notes/noteapp/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Note
 
 # Register your models here.
-admin.site.register(Note)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'created', 'updated']
+    list_filter = ['category', 'created', 'updated']
+    search_fields = ['title', 'body']
+    readonly_fields = ['created', 'updated']
+    ordering = ['-created']
