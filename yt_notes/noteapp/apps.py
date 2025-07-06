15709e65ddd5ecca26fe5558ed683bcaed0a4a47
kibedijoshua <kibedijoshua7@gmail.com>
2025-07-06 18:26:40 +0300

    It was importing the Note model at the module level, which causes a circular import It was trying to register the admin at the module level, which should be done in admin.py There was a missing space in the import statement

--- yt_notes/noteapp/apps.py
+++ yt_notes/noteapp/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.contrib import admin
-from . models import Note
 
 
 class NoteappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'noteapp'
-    
-    
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ["title", "category", "created", "updated"]
-admin.site.register(Note) 
+    name = 'noteapp' 
